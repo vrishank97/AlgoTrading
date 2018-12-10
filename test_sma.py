9981acd9e3c581cea6d5101685b67e691f33d6bb
Vrishank Bhardwaj <vrishank1997@gmail.com>
2018-12-10 20:26:05 +0530

    create test_sma.py

---
+++ test_sma.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from backtest import Backtest
+from sma_agent import SMA_Agent
+import matplotlib.pyplot as plt
+import numpy as np
+
+prices = pd.read_csv("Historical data/DABU Historical Data.csv")["Price"]
+
+agent = SMA_Agent(10000, 10, 0.015, 0.015)
+
+test = Backtest(agent)
+
+output = test.run(prices)
+
+fig, ax = plt.subplots()
+ax.plot(np.arange(len(prices)), output, color='green')
+ax.plot(np.arange(len(prices)), prices, color='green')
+
+ax.set(xlabel='Days', ylabel='INR',
+       title='Monies')
+ax.grid()
+plt.show()
